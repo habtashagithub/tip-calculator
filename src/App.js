f173f1dfeb67855ceb776818d4ed2cfc3852fccf
habtashagithub <habtamuashambel@gmail.com>
2024-03-05 07:00:44 -0800

    help to calculate tip

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import "./App.css";
+export default function App() {
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+  const tip = (bill * (percentage1 + percentage2)) / 2 / 100;
+
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
+  return (
+    <div>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
+        How did you like the service
+      </SelectPercentage>
+      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
+        how did your friend like the service
+      </SelectPercentage>
+      <OutPut bill={bill} tip={tip} />
+      <Reset onReset={handleReset} />
+    </div>
+  );
+}
+function BillInput({ bill, onSetBill }) {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input
+        type="text"
+        placeholder="bill"
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+function SelectPercentage({ children, percentage, onSelect }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select
+        value={percentage}
+        onChange={(e) => onSelect(Number(e.target.value))}
+      >
+        <option value="0">Dissatisfied(0%)</option>
+        <option value="5">it was okay(5%)</option>
+        <option value="10">it was good(10%)</option>
+        <option value="20">absolutly amazing!(20%)</option>
+      </select>
+    </div>
+  );
+}
+function OutPut({ bill, tip }) {
+  return (
+    <div>
+      <h2>
+        you pay {bill + tip} (${bill} + ${tip} tip)
+      </h2>
+    </div>
+  );
+}
+function Reset({ onReset }) {
+  return (
+    <div>
+      <button onClick={onReset}>reset</button>
+    </div>
+  );
+}
